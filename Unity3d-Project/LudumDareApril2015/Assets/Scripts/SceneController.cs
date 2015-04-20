c6ff5fdef61b833a6809ebc9f90933353ae9dc39
Eduardo Fonseca <efonsecab@gmail.com>
2015-04-19 19:50:23 -0600

    progress on screens

---
+++ Unity3d-Project/LudumDareApril2015/Assets/Scripts/SceneController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneController : MonoBehaviour
+{
+    public AudioSource PlayAudio;
+    private bool HastToStartGame = false;
+    private float SecondsElapsedSinceStartRequest = 0;
+    private float SecondsOnStartRequest = 0;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SecondsElapsedSinceStartRequest = Time.time - SecondsOnStartRequest;
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            StartGame();
+        }
+        if (this.HastToStartGame && SecondsElapsedSinceStartRequest > 2.5)
+        {
+            Application.LoadLevel("GameScene");
+        }
+    }
+
+    public void StartGame()
+    {
+        this.SecondsOnStartRequest = Time.time;
+        PlayAudio.Play();
+        this.HastToStartGame=true;
+    }
+}
